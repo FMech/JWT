ba16c11a1f31dd7417ff6ce08984b1434f7038cc
farah mechichi <mechichif141@gmail.com>
2022-12-22 13:05:47 +0100

    first

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const secretKey="fahara"
+const jwbt = require('jsonwebtoken')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+const data = [
+    {
+        film: "and then there were none",
+        year: 1997,
+    }, {
+        film: "gone with the wind",
+        year: 1995,
+
+    },
+]
+
+
+app.get("/data/:id",  (req, res) => {
+    if(req.params.id == secretCode ){
+        res.json(data)
+    }else{
+        res.json({err: "vous n'etes pas autorisé pour effectuer cette tache "})
+    }
+});
+  
+function createToken(req,res,next){
+    const user = {username:req.body.username}
+    jwbt.sign(user,secretKey,(err,result)=>{
+        if(err){
+            res.json({error:err})
+        }else{
+            res.json({token:result})
+        }
+ 
+    });
+    next();
+}
+
+app.post('/login',createToken,(req,res)=>{});
+
+app.listen(5000, () => {
+    console.log("the server is working")
+});
